befee8ebffd428b683c68a0ebb8b6b007ea60378
golit5 <xdgolits@gmail.com>
2024-05-08 16:19:58 +0300

    aboba

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -3,6 +3,12 @@ import './App.css';
 import Button from '@mui/material/Button';
 import UnivRating from './Table'
 
+async function fetchAsync (url = "https://localhost:5000/") {
+              let response = await fetch(url);
+              let data = await response.json();
+              return data;
+            }
+
 function App() {
   return (
     <div className="App">
@@ -11,7 +17,9 @@ function App() {
         
       </header>
       <body>
-          <Button variant="contained">Hello world</Button> 
+          <Button variant="contained"
+          onClick={() => {
+            fetchAsync ("https://localhost:5000/user") }}>Hello world</Button> 
           <UnivRating />
       </body>
     </div>
--- react-app/src/Table.js
+++ react-app/src/Table.js
@@ -6,26 +6,31 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Typography from '@mui/material/Typography';
 import Toolbar from '@mui/material/Toolbar';
+import { TableContainer } from '@mui/material';
+import Paper from '@mui/material/Paper';
 
 class Row {
-    constructor(id_, perf_, infrstr_, curm_, emplt_ = null, sci_ = null, intnat_ = null){
+    constructor(id_, perf_, infrstr_, curm_, sci_ = null, intnat_ = null){
         this.id = id_;
         this.perf = perf_;
         this.infrstr = infrstr_;
         this.curm = curm_;
-        this.emplt = emplt_;
         this.sci = sci_;
         this.intnat = intnat_;
     }
 
     get items(){
-        return this.id, this.perf, this.infrstr, this.curm, this.emplt, this.sci, this.intnat;
+        return this.id, this.perf, this.infrstr, this.curm, this.sci, this.intnat;
+    }
+
+    get rating(){
+      return this.perf + this.infrstr + this.curm + this.sci + this.intnat;
     }
 };
 
 const rows = [
   new Row("R.E.Alexeev NNSTU", 7, 7, 7),
-  new Row("Lobach", 0, 0, 0, 0, 0, 0)
+  new Row("Lobach", 0, 0, 0, 0, 0)
 ];
 
 function preventDefault(event) {
@@ -34,28 +39,28 @@ function preventDefault(event) {
 
 export default function UnivRating() {
   return (
-    <React.Fragment>
+    <TableContainer component={Paper}>
       <Toolbar>
             <Typography
               component="h1"
               variant="h6"
-              color="inherit"
+              color="primary"
               noWrap
               sx={{ flexGrow: 1 }}
             >
               N
             </Typography>
           </Toolbar>
-      <Table size="small">
+      <Table size="small" className="UnivRatingTable">
         <TableHead>
           <TableRow>
             <TableCell>Название</TableCell>
             <TableCell>Успеваемость</TableCell>
             <TableCell>Инфраструктура</TableCell>
             <TableCell>Учебная программа</TableCell>
-            <TableCell>Трудоустройство</TableCell>
             <TableCell>Наука</TableCell>
-            <TableCell align="right">Междунар. связи</TableCell>
+            <TableCell>Междунар. связи</TableCell>
+            <TableCell>Сумма</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
@@ -65,13 +70,13 @@ export default function UnivRating() {
               <TableCell>{row.perf}</TableCell>
               <TableCell>{row.infrstr}</TableCell>
               <TableCell>{row.curm}</TableCell>
-              <TableCell>{row.emplt}</TableCell>
               <TableCell>{row.sci}</TableCell>
-              <TableCell align="right">{row.intnat}</TableCell>
+              <TableCell>{row.intnat}</TableCell>
+              <TableCell>{row.rating}</TableCell>
             </TableRow>
           ))}
         </TableBody>
       </Table>
-    </React.Fragment>
+    </TableContainer>
   );
 }
